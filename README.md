1f0a426452e023cb4bad0f49d97075b9b450b88d
justinjasmann <justin.jasmann@gmail.com>
2012-11-28 03:45:52 -0500

    Adding some error checking: too many/little arguments passed & adding helpful results to the console. Also updating README.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-count_lines
-===========
+#Count Lines
+***
+##Overview
+Hello Visitors!
+
+I created this simple Python script to count the number of lines of files residing within a directory (or sub directory) that match certain file extensions.
+
+###Description
+
+If you'd like, you can modify the global variable "file_extensions" to include or exclude various file extensions to match on.
+	
+	file_extensions = [".java", "xml"]
+	
+###Run
+
+To run it, feed this script to the Python interpreter with whichever root directory you'd like to start the count from.
+
+	python count_lines.py $HOME/workspace/test_dir
+	
+Results will be shown in the console.
+	
+	directory "$HOME/workspace/test_dir"
+	100 files with extensions matching ['.java', 'xml']
+	5000 lines counted
+	
+###Feedback
+
+Keep in mind, I am very new to Python development, but if you have any suggestions for improvement, I'd love to hear them.
+
+###Contact
+
+Justin Jasmann  
+<justin.jasmann@gmail.com>
--- src/count_lines.py
+++ src/count_lines.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+file_extensions = [".java", "xml"]
+
 def count_lines(fname):
 	f = open(fname, 'r')
 	return len(f.readlines())
@@ -9,18 +11,42 @@ def find_java_or_xml(rootdir):
 	java_or_xml = []
 	for dirpath, dirnames, filenames in os.walk(rootdir):
 		for file in filenames:
-			if file.endswith(".xml") or file.endswith(".java"):
-				java_or_xml.append(dirpath + "/" + file)
+			for file_ext in file_extensions:
+				if file.endswith(file_ext):
+					java_or_xml.append(dirpath + "/" + file)
+					break;
 	return java_or_xml
 
-def total_line_count():
+def total_line_count(dirname):
 	total = 0
-	for fname in find_java_or_xml(sys.argv[1]):
-		total += count_lines(fname)
+	java_or_xml_files = find_java_or_xml(dirname)
+	if len(java_or_xml_files) == 0:
+		print num_files_found_message(0)
+	else:
+		print num_files_found_message(len(java_or_xml_files))
+		for fname in java_or_xml_files:
+			total += count_lines(fname)
 	return total
 
-print "\n"
-print "**********************************************"
-print "total lines writen (.java & .xml): " + str(total_line_count())
-print "**********************************************"
-print "\n"
+def num_files_found_message(num_files):
+	msg = str(num_files) + " files matched extensions: " + str(file_extensions)
+	return indent_msg(msg)
+
+def indent_msg(msg):
+	return "\t" + msg
+
+def main(dirname):
+	print indent_msg("directory \"" + str(dirname) + "\"")
+	print indent_msg(str(total_line_count(dirname)) + " total lines counted")
+	
+if __name__ == "__main__":
+	print 
+	if len(sys.argv) == 2:
+		directory = sys.argv[1]
+		if os.path.isdir(directory) == True:
+			main(directory)
+		else:
+			print indent_msg("\"" + directory + "\" is not a directory")
+	else:
+		print indent_msg("Usage: python count_lines.py <directory_with_files>")
+	print 
